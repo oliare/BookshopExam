974882e3dff80585d60b585e6b38e2b3407fc205
Olia <oliaopanasiukk@gmail.com>
2024-04-02 13:15:55 +0300

    add project files

--- Bookshop/Bookshop/EditPage.xaml.cs
+++ Bookshop/Bookshop/EditPage.xaml.cs
@@ -14,7 +14,10 @@ namespace Bookshop
     public partial class EditPage : Page
     {
         private static BookshopDb context = BookshopDb.Create();
+<<<<<<< HEAD
+=======
         private string _table;
+>>>>>>> 9f6134b44aaecbd8af703c0037a0e5082d9b87a9
         private static User User { get; set; }
         private static Book Book { get; set; }
         public EditPage(User? user)
@@ -35,22 +38,42 @@ namespace Bookshop
         {
             grid.HeadersVisibility = DataGridHeadersVisibility.All;
             grid.ItemsSource = DbManager.GetBooks();
+<<<<<<< HEAD
+            mode = EditMode.Books;
+=======
             mode = DeleteMode.Books;
+>>>>>>> 9f6134b44aaecbd8af703c0037a0e5082d9b87a9
         }
         private void viewUsers_Click(object sender, RoutedEventArgs e)
         {
             grid.HeadersVisibility = DataGridHeadersVisibility.All;
             grid.ItemsSource = DbManager.GetUsers();
+<<<<<<< HEAD
+            mode = EditMode.Users;
+=======
+>>>>>>> 9f6134b44aaecbd8af703c0037a0e5082d9b87a9
         }
         private void viewDiscounts_Click(object sender, RoutedEventArgs e)
         {
             grid.HeadersVisibility = DataGridHeadersVisibility.All;
             grid.ItemsSource = DbManager.GetDiscounts();
+<<<<<<< HEAD
+            mode = EditMode.Discounts;
+=======
+>>>>>>> 9f6134b44aaecbd8af703c0037a0e5082d9b87a9
         }
         private void viewPurchasedBooks_Click(object sender, RoutedEventArgs e)
         {
             grid.HeadersVisibility = DataGridHeadersVisibility.All;
             grid.ItemsSource = DbManager.GetUserPurchasedBooks();
+<<<<<<< HEAD
+            mode = EditMode.PurchasedBooks;
+        }
+        private void back_Click(object sender, RoutedEventArgs e)
+        {
+            var hasChanges = context.ChangeTracker.HasChanges();
+            if (hasChanges)
+=======
         }
         private void back_Click(object sender, RoutedEventArgs e)
         {
@@ -58,6 +81,7 @@ namespace Bookshop
             
             var hasChanges = context.ChangeTracker.HasChanges();
             if (hasChanges != null)
+>>>>>>> 9f6134b44aaecbd8af703c0037a0e5082d9b87a9
             {
                 MessageBoxResult result = MessageBox.Show("Save changes before leaving this page?", "Confirmation", MessageBoxButton.YesNoCancel);
                 if (result == MessageBoxResult.Cancel)
@@ -67,12 +91,20 @@ namespace Bookshop
                 else if (result == MessageBoxResult.Yes)
                 {
                     context.SaveChanges();
+<<<<<<< HEAD
+                    MessageBox.Show("Changes saved successfully!");
+=======
                     MessageBox.Show("Changes saved successfully");
+>>>>>>> 9f6134b44aaecbd8af703c0037a0e5082d9b87a9
                 }
             }
             NavigationService.Navigate(new AdminPage(null));
         }
+<<<<<<< HEAD
+        public enum EditMode
+=======
         public enum DeleteMode
+>>>>>>> 9f6134b44aaecbd8af703c0037a0e5082d9b87a9
         {
             None,
             Books,
@@ -80,6 +112,70 @@ namespace Bookshop
             Users,
             PurchasedBooks
         }
+<<<<<<< HEAD
+        private EditMode mode = EditMode.None;
+
+        private void delete_Click(object sender, RoutedEventArgs e)
+        {
+            switch (mode)
+            {
+                case EditMode.Books:
+                    var booksToDelete = grid.SelectedItems.OfType<Book>().ToList();
+                    context.Books.RemoveRange(booksToDelete);
+                    break;
+                case EditMode.Discounts:
+                    var discountsToDelete = grid.SelectedItems.OfType<Discount>().ToList();
+                    context.Discounts.RemoveRange(discountsToDelete);
+                    break;
+                case EditMode.Users:
+                    var usersToDelete = grid.SelectedItems.OfType<User>().ToList();
+                    context.Users.RemoveRange(usersToDelete);
+                    break;
+                case EditMode.PurchasedBooks:
+                    var purchasedBooksToDelete = grid.SelectedItems.OfType<PurchasedBook>().ToList();
+                    context.PurchasedBooks.RemoveRange(purchasedBooksToDelete);
+                    break;
+                default:
+                    MessageBox.Show("Please select an edit mode");
+                    return;
+            }
+
+            MessageBox.Show("Deletion is successful!");
+            context.SaveChanges();
+        }
+        private void add_Click(object sender, RoutedEventArgs e)
+        {
+            grid.ItemsSource = null;
+            switch (mode)
+            {
+                case EditMode.Books:
+                    var book = new Book();
+                    grid.Items.Add(book);
+                    DbManager.AddNewBook(book);
+                    break;
+                case EditMode.Discounts:
+                    var disc = new Discount();
+                    grid.Items.Add(disc);
+                    DbManager.AddNewDiscount(disc);
+                    break;
+                case EditMode.Users:
+                    var user = new User();
+                    grid.Items.Add(user);
+                    DbManager.AddNewUser(user); 
+                    break;
+                case EditMode.PurchasedBooks:
+                    var pb = new PurchasedBook();
+                    grid.Items.Add(pb);
+                    DbManager.AddNewPurchasedBook(pb); 
+                    break;
+                default:
+                    MessageBox.Show("Please select an edit mode");
+                    return;
+            }
+
+            MessageBox.Show("New item added successfully!");
+        }
+=======
         private DeleteMode mode = DeleteMode.None;
 
         private void delete_Click(object sender, RoutedEventArgs e)
@@ -87,13 +183,18 @@ namespace Bookshop
             MessageBox.Show("Deletion is successful");
             context.SaveChanges();
         }
+>>>>>>> 9f6134b44aaecbd8af703c0037a0e5082d9b87a9
 
         private void saveChanges_Click(object sender, RoutedEventArgs e)
         {
             try
             {
                 var hasChanges = context.ChangeTracker.HasChanges();
+<<<<<<< HEAD
+                if (hasChanges)
+=======
                 if (hasChanges != null)
+>>>>>>> 9f6134b44aaecbd8af703c0037a0e5082d9b87a9
                 {
                     context.SaveChanges();
                     MessageBox.Show("Changes saved successfully");
@@ -115,6 +216,23 @@ namespace Bookshop
         {
             switch (mode)
             {
+<<<<<<< HEAD
+                case EditMode.Books:
+                    var books = grid.SelectedItems.OfType<Book>().ToList();
+                    context.Books.UpdateRange(books);
+                    break;
+                case EditMode.Discounts:
+                    var discounts = grid.SelectedItems.OfType<Discount>().ToList();
+                    context.Discounts.UpdateRange(discounts);
+                    break;
+                case EditMode.Users:
+                    var users = grid.SelectedItems.OfType<User>().ToList();
+                    context.Users.UpdateRange(users);
+                    break;
+                case EditMode.PurchasedBooks:
+                    var purchasedBooks = grid.SelectedItems.OfType<PurchasedBook>().ToList();
+                    context.PurchasedBooks.UpdateRange(purchasedBooks);
+=======
                 case DeleteMode.Books:
                     var books = grid.SelectedItems.OfType<Book>().ToList();
                     context.Books.UpdateRange(books);
@@ -130,13 +248,17 @@ namespace Bookshop
                 case DeleteMode.PurchasedBooks:
                     var purchasedBooks = grid.SelectedItems.OfType<PurchasedBook>().ToList();
                     context.PurchasedBooks.RemoveRange(purchasedBooks);
+>>>>>>> 9f6134b44aaecbd8af703c0037a0e5082d9b87a9
                     break;
                 default:
                     MessageBox.Show("Please select a delete mode");
                     return;
             }
+<<<<<<< HEAD
+=======
 
 
+>>>>>>> 9f6134b44aaecbd8af703c0037a0e5082d9b87a9
         }
     }
 }
--- BookshopDAL/BookshopDAL/DbManager.cs
+++ BookshopDAL/BookshopDAL/DbManager.cs
@@ -20,6 +20,50 @@ namespace Bookshop.DAL
             context.SaveChanges();
             return newUser.Entity;
         }
+<<<<<<< HEAD
+
+        public static Book AddNewBook(Book book)
+        {
+            //var newBook = new Book
+            //{
+            //    Title = book.Title,
+            //    Author = book.Author,
+            //    Publisher = book.Publisher,
+            //    Pages = book.Pages,
+            //    Genre = book.Genre,
+            //    Year = book.Year,
+            //    CostPrice = book.CostPrice,
+            //    SellingPrice = book.SellingPrice,
+            //    IsContinuation = book.IsContinuation,
+            //    Quantity = book.Quantity,
+            //    DiscountId = book.DiscountId,
+            //    IsReserved = book.IsReserved
+            //};
+
+            var newBook = context.Books.Add(book);
+            context.SaveChanges();
+            return newBook.Entity;
+        }
+        public static User AddNewUser(User user)
+        {
+            var newUser = context.Users.Add(user);
+            context.SaveChanges();
+            return newUser.Entity;
+        }
+        public static Discount AddNewDiscount(Discount discount)
+        {
+            var newDisc = context.Discounts.Add(discount);
+            context.SaveChanges();
+            return newDisc.Entity;
+        }
+        public static PurchasedBook AddNewPurchasedBook(PurchasedBook pb)
+        {
+            var newPb = context.PurchasedBooks.Add(pb);
+            context.SaveChanges();
+            return newPb.Entity;
+        }
+=======
+>>>>>>> 9f6134b44aaecbd8af703c0037a0e5082d9b87a9
         public static void BuyBook(Book book, User user)
         {
             context.PurchasedBooks.Add(new PurchasedBook() { User = user, Book = book });
@@ -75,6 +119,18 @@ namespace Bookshop.DAL
         {
             return context.Books.Where(b => b.Year >= year).OrderByDescending(b => b.Year).ToList();
         }
+<<<<<<< HEAD
+
+        public static List<Book> GetPopularBooks(int number)
+        {
+            return context.PurchasedBooks
+                    .GroupBy(pb => pb.Book)
+                    .Select(g => new { Book = g.Key, SalesCount = g.Count() })
+                    .OrderByDescending(g => g.SalesCount)
+                    .Take(number)
+                    .Select(g => g.Book)
+                    .ToList();
+=======
       
         public static List<Book> GetPopularBooks(int number)
         {
@@ -85,6 +141,7 @@ namespace Bookshop.DAL
                    .Take(number)
                    .Select(g => g.Book)
                    .ToList();
+>>>>>>> 9f6134b44aaecbd8af703c0037a0e5082d9b87a9
         }
 
         public static List<Book> GetPopularGenres()
